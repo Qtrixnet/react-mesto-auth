90a47888e5ba8781c77f04c4097043a0d6edbb7c
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-03-07 14:51:29 +0600

    feat: добавить бургер меню и проверку ширины экрана

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../images/Logo-white.svg";
 import { Link, Route } from 'react-router-dom';
 
 function Header({ email, onSignOut }) {
+  //* Состояния
   const [isClicked, setIsClicked] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  //* Проверка ширины окна просмотра, если больше 767 то бургер-меню считается открытым. 
+  //* Без контроля ширины окна просмотра, если в мобильной версии закрыть бургер меню и развернуть большой экран, то поле имейла и кнопка имеют display: none;
+  let resizeWindow = () => {
+    setWindowWidth(window.innerWidth);
+    if(windowWidth >= 767) {
+      setIsClicked(true);
+    } 
+  };
+
+ //* Проверка ширины окна при монтировании компонентов
+  useEffect(() => {
+    resizeWindow();
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  }, [resizeWindow]);
 
   function handleClickMenu() {
     setIsClicked(!isClicked)
   }
-  
+
   return (
     <header className="header">
       <img className="header__logo" alt="Логотип" src={logo} />
@@ -19,16 +37,21 @@ function Header({ email, onSignOut }) {
         <Link to='sign-in' className='header__link'>Войти</Link>
       </Route>
       <Route exact path="/">
-            <div className="header__user-menu">
-              <p className='header__email'>{email}</p>
-              <button
-                onClick={() => {
-                  onSignOut();
-                  handleClickMenu();
-                }}
-                className='header__link header__link_button'>Выйти</button>
-            </div>
-        </Route>
+        <div className='header__burger-menu'>
+          <button type="button" className={`${isClicked ? 'header__button header__button_close' : 'header__button header__button_burger'}`} onClick={handleClickMenu}>
+          <span></span>
+          </button>
+        </div>
+        <div className={`${isClicked ? 'header__user-menu' : 'not-active'}`}>
+          <p className='header__email'>{email}</p>
+          <button
+            onClick={() => {
+              onSignOut();
+              handleClickMenu();
+            }}
+            className='header__link header__link_button'>Выйти</button>
+        </div>
+      </Route>
     </header>
   )
 }
